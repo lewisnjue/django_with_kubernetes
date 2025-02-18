8e797f31829a66f9ceace443bc05c4e26c3e6f80
lewisnjue <lewiskinyuanjue.ke@gmail.com>
2025-02-18 20:43:53 +0300

    migrate.sh

---
+++ migrate.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+DJANGO_SUPERUSER_EMAIL =${DJANGO_SUPERUSER_EMAIL:-"testing@gmail.com"}
+
+cd /app/
+
+/opt/env/bin/python manage.py migrate --noinput
+/opt/env/bin/python manage.py createsuperuser --email $DJANGO_SUPERUSER_EMAIL --noinput || true
+
